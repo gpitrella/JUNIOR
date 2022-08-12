cb7e775c04ea011db07556fa1a2c98cdbead572e
gpitrella <gabriel98_@hotmail.com>
2022-08-12 17:12:32 -0300

    dataBase created

--- api/src/config.js
+++ api/src/config.js
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/notesdb";
+export const PORT = process.env.PORT || 4001;
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/juniordb";
--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,17 @@
 import app from "./app.js";
 import { connectDB } from "./database.js";
 import { createAdminUser } from "./libs/createUser.js";
+import { PORT } from "./config.js";
 
 async function main() {
   await connectDB();
   await createAdminUser();
   app.listen(app.get("port"));
 
-  console.log("Server on port", app.get("port"));
+  console.log(`Server on port: ${PORT}, Running ...`, app.get("port")); 
   console.log("Environment:", process.env.NODE_ENV);
 }
+
+
 
 main();
