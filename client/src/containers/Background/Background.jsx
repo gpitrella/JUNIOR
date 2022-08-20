1a63c78a026edf334a6330df0da0eae8de46f06a
gpitrella <gabriel98_@hotmail.com>
2022-08-20 10:32:50 -0300

    update Landing Page

--- client/src/containers/Background/Background.jsx
+++ client/src/containers/Background/Background.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Background.scss';
+import logo from "../../assets/Logo-JR.jpg"
 
 /**
  * Features of GPT3
@@ -7,29 +8,40 @@ import './Background.scss';
  */
 const Background = () => {
 	return (
-        <>
+        <div>
 		<div id="stars"></div>
         <div id="stars2"></div>
         <div id="stars3"></div>
+            
         <div id="horizon">
             <div className="glow"></div>
         </div>
-        <div id="earth"></div>
+        <div id="earth">
+        </div>
             {/* <div id="title">
                 GRAVITY
-            </div>
-            <div id="subtitle">
-                <span>
-                    DONT
-                </span>
-                <span>
-                    LET
-                </span>
-                <span>
-                    GO
-                </span>
             </div> */}
-        </>
+            <div id="footer_link">
+                <div id="subtitle">
+                    <span>
+                        LISTO <span></span>
+                    </span>
+                    <span>
+                        PARA <span></span>
+                    </span>
+                    <span>
+                        EMPEZAR
+                    </span>
+                </div>
+                <div id="footer_main_links">
+                    <h5>Contacto</h5>
+                    <h5>Proyectos</h5>
+                    <img src={logo} alt="logo Junior footer" /> 
+                    <h5>Finalizados</h5>
+                    <h5>Nosotros</h5>
+                </div>
+            </div>
+        </div>
 	)
 }
 
