6e0a509a49cfa0f8daed45ffaf1f52951296fcab
gpitrella <gabriel98_@hotmail.com>
2022-09-21 15:41:36 -0300

    cimprementación passport Local

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 import { useSelector } from "react-redux";
+import MenuItem from '@mui/material/MenuItem';
 import './NavBar.css';
 import logo from '../../assets/logo.png';
 
@@ -11,10 +12,10 @@ const Menu = () => {
 		<>
 			<Link to='/projects'><p>Proyectos</p></Link>
 			<p><a href="#features">About JR</a></p>
-			{/* <p><a href="#possibility">Open AI</a></p>
-			<p><a href="#blog">Library</a></p> */}
 		</>
 )}
+
+
 
 const SignBtns = ({handleSignOut, user}) => {
 	
@@ -27,6 +28,7 @@ const SignBtns = ({handleSignOut, user}) => {
 				: ( <>
 						<p>Bienvenido {user?.user?.name}</p>
 						<button className="btn" type="button" onClick={(e) => handleSignOut(e)}>LogOut</button>
+						<button className="btn" type="button" onClick={(e) => handleSignOut(e)}>Mi Perfil</button>
 				  	</>)
 			}
 	
@@ -41,6 +43,8 @@ export default function Navbar ({ handleSignOut }) {
 	const [toggleMenu, setToggleMenu] = useState(false) // Use for controll the mini menu
 	const [stickyNavbar, setStickyNavbar] = useState(false)
 	const { user } = useSelector((state) => state.homepageReducer);
+
+	
 	window.onscroll = function() {scrollFunction()};
 	function scrollFunction() {
 		if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -48,7 +52,7 @@ export default function Navbar ({ handleSignOut }) {
 		} else {
 			setStickyNavbar(false);
 		}
-	  }
+	}	
 
 	return (
 		<div className={ stickyNavbar ? "navbarSticky" : "navbar" }>
@@ -66,7 +70,7 @@ export default function Navbar ({ handleSignOut }) {
 			</div>
 
 			<div className="navbar-sign">
-				<SignBtns handleSignOut={handleSignOut} user={user}/>
+				<SignBtns handleSignOut={handleSignOut} user={user} />
 			</div>
 
 			<div className="navbar-menu">
