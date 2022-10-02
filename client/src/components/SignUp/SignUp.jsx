0fc4ed0f9818aacde1637cede5a4c48fdf4c4413
gpitrella <gabriel98_@hotmail.com>
2022-10-02 09:48:49 -0300

    update Error SignUp

--- client/src/components/SignUp/SignUp.jsx
+++ client/src/components/SignUp/SignUp.jsx
@@ -17,32 +17,40 @@ export default function SignUp() {
         password: '',
         password2:''
     })
-    // const { logInError } = useSelector((state) => state.general)
+    const { logInError } = useSelector((state) => state.homepageReducer)
 
-    const [errors, setErrors] = useState({})
-    const [errorsEmail, setErrorsEmail] = useState({})
-    const [errorsPasword, setErrorsPassword] = useState({})
-    const [errorsPassword2, setErrorsPassword2] = useState({})
+    const [errors, setErrors] = useState({
+        username: "Add a username",
+        email: "Add an email",
+        password: "Add a password",
+        password2: "Repeat password"
+    })
+    // const [errorsEmail, setErrorsEmail] = useState({})
+    // const [errorsPasword, setErrorsPassword] = useState({})
+    // // const [errorsPassword2, setErrorsPassword2] = useState({})
     const [openEmail, setOpenEmail] = useState(false);
 
     const handleChange = (e) => {
         e.preventDefault();
         setInput({...input,[e.target.name]: e.target.value})
-        setErrors(validateUsername({...input,[e.target.name]: e.target.value}))
-        setErrorsEmail(validateEmail({...input,[e.target.name]: e.target.value}))
-        setErrorsPassword(validatePassword({...input,[e.target.name]: e.target.value}))
-        setErrorsPassword2(validatePassword2({...input,[e.target.name]: e.target.value}))
+        setErrors(validateUsername({...input,[e.target.name]: e.target.value}, errors))
+        setErrors(validateEmail({...input,[e.target.name]: e.target.value}, errors))
+        setErrors(validatePassword({...input,[e.target.name]: e.target.value}, errors))
+        setErrors(validatePassword2({...input,[e.target.name]: e.target.value}, errors))
+        console.log(errors)
     }
 
     const dispatch = useDispatch();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(signUp(input.username, input.email, input.password, input.password2))
-        setErrors(validateUsername({...input,[e.target.name]: e.target.value}))
-        setErrorsEmail(validateEmail({...input,[e.target.name]: e.target.value}))
-        setErrorsPassword(validatePassword({...input,[e.target.name]: e.target.value}))
-        setErrorsPassword2(validatePassword2({...input,[e.target.name]: e.target.value}))
+        setErrors(validateUsername({...input,[e.target.name]: e.target.value}, errors))
+        setErrors(validateEmail({...input,[e.target.name]: e.target.value}, errors))
+        setErrors(validatePassword({...input,[e.target.name]: e.target.value}, errors))
+        setErrors(validatePassword2({...input,[e.target.name]: e.target.value}, errors))
+        if(Object.keys(errors).length === 0) {
+            dispatch(signUp(input.username, input.email, input.password, input.password2))
+        }
       }
 
       const handleCloseEmail = (event, reason) => {
@@ -52,16 +60,23 @@ export default function SignUp() {
         setOpenEmail(false);
       };
 
-    //   React.useEffect(() => {
-    //     // console.log(logInError)
-    //     if(logInError.status === 404){
-    //         setOpenEmail(true)
-    //         errorsEmail.email = "Email aready used"
-    //         document.getElementById('email').classList.add('signup__group-incorrecto')
-    //         document.getElementById('email').classList.remove('signup__group-correcto')
-    //         document.querySelector('#email .signup__input-error').classList.add('signup__input-error-activo')
-    //     }
-    //   },[logInError]); 
+      React.useEffect(() => {
+        // console.log(logInError)
+        if(logInError.status === 404){
+            setOpenEmail(true)
+            errors.email = logInError?.data.msg
+            document.getElementById('email').classList.add('signup__group-incorrecto')
+            document.getElementById('email').classList.remove('signup__group-correcto')
+            document.querySelector('#email .signup__input-error').classList.add('signup__input-error-activo')
+        }
+        if(logInError.status === 401){
+            setOpenEmail(true)
+            errors.password = logInError?.data.msg
+            document.getElementById('email').classList.add('signup__group-incorrecto')
+            document.getElementById('email').classList.remove('signup__group-correcto')
+            document.querySelector('#email .signup__input-error').classList.add('signup__input-error-activo')
+        }
+      },[logInError]); 
     
     return (
 
@@ -95,7 +110,7 @@ export default function SignUp() {
                     onChange={(e) => handleChange(e)}
                     className = {`signup__input ${s.input}`}
                     />
-                    <p className = {`signup__input-error ${s.errorMsg}`}>{errorsEmail.email}</p>
+                    <p className = {`signup__input-error ${s.errorMsg}`}>{errors.email}</p>
                 </div>
 
                 <div className='signup__group' id='password'>
@@ -107,7 +122,7 @@ export default function SignUp() {
                     onChange={(e) => handleChange(e)}
                     className = {`signup__input ${s.input}`}
                     />
-                    <p className = {`signup__input-error ${s.errorMsg}`}>{errorsPasword.password}</p>
+                    <p className = {`signup__input-error ${s.errorMsg}`}>{errors.password}</p>
                 </div>
 
                 <div className='signup__group' id='password2'>
@@ -119,7 +134,7 @@ export default function SignUp() {
                     onChange={(e) => handleChange(e)}
                     className = {`signup__input ${s.input}`}
                     />
-                    <p className = {`signup__input-error ${s.errorMsg}`}>{errorsPassword2.password2}</p>
+                    <p className = {`signup__input-error ${s.errorMsg}`}>{errors.password2}</p>
                 </div>
                 
                 <div className='signup__group' >
