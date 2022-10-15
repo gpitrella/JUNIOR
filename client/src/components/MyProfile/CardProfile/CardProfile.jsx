7ca7522d67f0d91cda76d42f576cb792c6c68a08
gpitrella <gabriel98_@hotmail.com>
2022-10-15 20:59:10 -0300

    update MiProfile

--- client/src/components/MyProfile/CardProfile/CardProfile.jsx
+++ client/src/components/MyProfile/CardProfile/CardProfile.jsx
@@ -1,27 +1,18 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import { Button, CardActionArea, CardActions } from '@mui/material';
-import { useLottie } from "lottie-react";
 import { Link } from 'react-router-dom';
 import s from './CardProfile.module.css';
 
 
-export default function CardProfile ({ lottie, name, link, width, marginLeft }) {
+export default function CardProfile ({ img, name, link, width }) {
 
-    // Lottie
-  const options = {
-    animationData: lottie,
-    loop: true
-  };
-
-  const { View } = useLottie(options);
-
-    return (
+   return (
         <Card id='individual_box_myprofile' sx={{ width: 200, height: 200, bgcolor: "rgba(32, 32, 36, 0.8)", borderRadius: 3 }}>
           <CardActionArea >
             <Link to={link}>
-              <div className={s.personal_informacion} style={{width: `${width}px`, marginLeft: `${marginLeft}px`}}>
-                  { View }
+              <div className={s.personal_informacion} style={{width: `100%`, textAlign: 'center'}}>
+                  <img src={img} alt={name} width={`${width}px`} ></img>
               </div>
             </Link>
           </CardActionArea>
