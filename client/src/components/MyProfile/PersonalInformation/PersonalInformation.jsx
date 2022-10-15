c2230c27337b2fd4f7520e4574ae165f4015f1b7
gpitrella <gabriel98_@hotmail.com>
2022-10-15 16:49:43 -0300

    update LocalStorage

--- client/src/components/MyProfile/PersonalInformation/PersonalInformation.jsx
+++ client/src/components/MyProfile/PersonalInformation/PersonalInformation.jsx
@@ -184,7 +184,7 @@ export default function PersonalInformation() {
     <div className='main_box_personalinformation'>
       <h1 className="gradient__text"> MI PERFIL </h1>
       <h2 className='title_personalinformation gradient__text'> Personal Information </h2>
-    <List className='box_main_personal' sx={{ width: '100%', maxWidth: 360, bgcolor: "rgba(32, 32, 36, 0.8)", borderRadius: 5, padding: 2 }}>
+    <List className='box_main_personal' sx={{ width: '100%', maxWidth: 500, bgcolor: "rgba(32, 32, 36, 0.8)", borderRadius: 5, padding: 2 }}>
       <ListItem id='avatar_personal_information'>
         <ListItemAvatar>
           <Avatar
