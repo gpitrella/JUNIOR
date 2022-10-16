528d71fda7d30b3079de831e02922e2105a2faf1
gpitrella <gabriel98_@hotmail.com>
2022-10-16 09:34:22 -0300

    update MiProfile

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Article } from './Article';
-export { default as Brand } from './Brand';
+export { default as Brand } from './Brand/Brand';
 export { default as CTA } from './CTA/CTA';
 export { default as Feature } from './Feature/Feature';
 export { default as Navbar } from './NavBar/NavBar';
