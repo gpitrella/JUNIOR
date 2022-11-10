5429e712dc67d16c82ce9e3a701c41fb0d087560
gpitrella <gabriel98_@hotmail.com>
2022-11-10 20:08:33 -0300

    update front

--- client/src/components/HallOfFameRows/HallOfFameRows.jsx
+++ client/src/components/HallOfFameRows/HallOfFameRows.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import HallOfFameRow from '../HallOfFameRow/HallOfFameRow';
+import { useSelector } from 'react-redux';
 import ModalInvitationProject from '../ModalInvitationProject/ModalInvitationProject';
+import Pagina from "../Pagina/Pagina.jsx";
 import s from './HallOfFameRows.module.css';
 
 export default function HallOfFameRows({ users }) {
+  const { pagina  } = useSelector(state => state.projectsReducer);
   
+  //? paginado
+  let currentPage = 0;
+  currentPage = pagina;
+  const maxpage = Math.ceil(users?.length / 3);
+
+  const developerToShow = () => {
+    const developerShow = users?.slice(
+      (currentPage - 1) * 25,
+      (currentPage - 1) * 25 + 25
+    );
+    return developerShow;
+  };
   
   return (
     <div className = {s.container}>
@@ -18,22 +33,27 @@ export default function HallOfFameRows({ users }) {
           y también su contacto para poder ubicarlos fuera de la aplicación.
 			</p>
       </div>
-      {
-        
-        users && users.length > 0 && users.map((user, index) => 
+      <div className={s.detailsProjects} >
+          <div className = {s.totalandfilterproject}>
+            <p> Total Developers: {users && users.length}	</p>
+            {/* <p> Projectos Filtrados: {allProjects.length}	</p> */}
+        </div>
+        <Pagina currentPage={currentPage} maxpage={maxpage}></Pagina>
+      </div>
+      {        
+        users && users.length > 0 && developerToShow().map((user, index) => 
 
           <HallOfFameRow
             id = {user.id}
             name = {user.name}
             collaborations = {user.collaborations}
             image = {user.image}
-            index = {index + 1}
+            index = {index + 1 + (25 * (currentPage - 1))}
             email = {user.email}
             linkedin = {user.linkedin}
             github = {user.github}
             key = {`hall-of-fame-user-${user.name}-${index}`}
           />
-
         )
       }
       <ModalInvitationProject />
