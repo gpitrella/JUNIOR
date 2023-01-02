e5f4dbfa3b9e5e2383721aea10adca79576281a3
Joseph Valenzuela <josephdavid200320@gmail.com>
2023-01-02 11:01:31 -0500

    Correcion de la pagina de inicio (NO sos Desarrollar)

--- client/src/containers/Possibility/Possibility.jsx
+++ client/src/containers/Possibility/Possibility.jsx
@@ -10,7 +10,7 @@ export default function Possibility () {
 					<img src={possibilityImg} alt="Person with idea" />
 				</div>
 				<div className={s.possibility_content}>
-					<h4>Tambien podes tener un proyecto sin ser Desarrollador</h4>
+					<h4>Tambien podes tener un proyecto sin ser <Desarrollador></Desarrollador></h4>
 					<h1 className='gradient__text'>¿NO sos Desarrollador? Pero tenes un proyecto para desarrollar.</h1>
 					<p>
 						Contactanos y vas a tener a los mejores Juniors disponibles para llevar tus proyectos a la realidad.
